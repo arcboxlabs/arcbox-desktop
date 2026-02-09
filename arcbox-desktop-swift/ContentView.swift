ef0622ebe8597edb9485f2bbe969ec99de344dd6
Raspberry <mashuo2855190178@icloud.com>
2026-02-09 22:46:55 +0800

    Initial Commit

---
+++ arcbox-desktop-swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  arcbox-desktop-swift
+//
+//  Created by 马硕 on 2026/2/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ arcbox-desktop-swift/arcbox_desktop_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  arcbox_desktop_swiftApp.swift
+//  arcbox-desktop-swift
+//
+//  Created by 马硕 on 2026/2/9.
+//
+
+import SwiftUI
+
+@main
+struct arcbox_desktop_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
